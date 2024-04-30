5ee358e62b23bde53775368437e5e5d125987bcd
Younkyum <jinyounkyum@hufs.ac.kr>
2024-04-30 15:07:59 +0900

    [Feat] OnveView 추가

---
+++ GitHub Practice 2024/GitHub Practice 2024/View/LetterViews/OnveView.swift
@@ -0,0 +1,21 @@
+//
+//  OnveView.swift
+//  GitHub Practice 2024
+//
+//  Created by YounkyumJin on 4/30/24.
+//
+
+import SwiftUI
+
+struct OnveView: LetterView {
+    var name: String = "Onve"
+    
+    var body: some View {
+        Text("세상을 지배하리라")
+        Text("-onve-")
+    }
+}
+
+#Preview {
+    OnveView()
+}
