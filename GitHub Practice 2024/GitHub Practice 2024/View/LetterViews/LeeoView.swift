910d6cf82c5d9b45d766825d0d4092ac24b548cb
seunggyun-jeong <jskyun98@gmail.com>
2024-05-01 16:49:05 +0900

    [Feat] 리이오 뷰 제작

---
+++ GitHub Practice 2024/GitHub Practice 2024/View/LetterViews/LeeoView.swift
@@ -0,0 +1,18 @@
+//
+//  LeeoView.swift
+//  GitHub Practice 2024
+//
+//  Created by Gyunni on 5/1/24.
+//
+
+import SwiftUI
+
+struct LeeoView: LetterView {
+    var body: some View {
+        Text("리이오의 다짐!@#!@")
+    }
+}
+
+#Preview {
+    LeeoView()
+}
