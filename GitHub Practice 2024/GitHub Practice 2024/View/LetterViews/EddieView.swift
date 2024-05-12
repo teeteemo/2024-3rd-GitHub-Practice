0e98453dab7200f9ad04a44a38c5cbd602788dfa
penum1227 <dodori1227@gmail.com>
2024-05-12 21:57:24 +0900

    add EddieView

---
+++ GitHub Practice 2024/GitHub Practice 2024/View/LetterViews/EddieView.swift
@@ -0,0 +1,20 @@
+//
+//  EddieView.swift
+//  GitHub Practice 2024
+//
+//  Created by jhon on 5/12/24.
+//
+
+import SwiftUI
+
+struct EddieView: LetterView {
+    var name: String = "Eddie"
+    
+    var body: some View {
+        Text("엘지 파이팅")
+    }
+}
+
+#Preview {
+    EddieView()
+}
