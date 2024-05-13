4ac0b64fd628bbad75ea6b121eb733c9a6909288
조민경 <32194293@dankook.ac.kr>
2024-05-13 11:10:40 +0900

    [Feat]{Pi}뷰 제작

---
+++ GitHub Practice 2024/GitHub Practice 2024/View/LetterViews/PiView.swift
@@ -0,0 +1,20 @@
+//
+//  PiView.swift
+//  GitHub Practice 2024
+//
+//  Created by ChoMinKyung on 5/13/24.
+//
+
+import SwiftUI
+
+struct PiView: LetterView {
+    var name: String = "Pi"
+    
+    var body: some View {
+        Text("할무니팀 파이팅")
+    }
+}
+
+#Preview {
+    PiView()
+}
