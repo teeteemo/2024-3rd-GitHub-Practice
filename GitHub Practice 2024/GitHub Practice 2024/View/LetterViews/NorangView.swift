441c9cea2fded6c683bf37e849b9c95ffb97cba6
norang <norang1221@gmail.com>
2024-05-13 10:58:44 +0900

    [Feat] NorangView 제작

---
+++ GitHub Practice 2024/GitHub Practice 2024/View/LetterViews/NorangView.swift
@@ -0,0 +1,18 @@
+//
+//  NorangView.swift
+//  GitHub Practice 2024
+//
+//  Created by Kyuhee hong on 5/13/24.
+//
+
+import SwiftUI
+
+struct NorangView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NorangView()
+}
